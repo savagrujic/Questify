9529cda88b3d5330289f274d4dbdf396866d71c1
savagrujic <savagrujic42@gmail.com>
2025-04-09 23:21:37 +0100

    added landing page

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -18,6 +18,7 @@ export default function DashBoard() {
     //Proveri Da li je url jednak sa stringom
     const isActive = (path) => location.pathname === path
 
+    //Postavi css activnog diva
     const navItem = (path) => `cursor-pointer ${isActive(path)
         ? 'py-2 text-gray-600 font-semibold flex items-center bg-blue-400 rounded-xl '
         : 'py-2 my-1 text-gray-600 font-semibold flex items-center hover:bg-blue-100 hover:rounded-xl duration-300 '
@@ -42,10 +43,10 @@ export default function DashBoard() {
         <div className='flex'>
             <div className='flex flex-col px-10 items-left  w-screen h-screen flex-1/5 border-r-2 border-gray-200' >
                <img className='w-40 my-5 ' src={logo} />
-               <div className= {navItem('/dashboard/home')}> <IoMdHome className='size-6 mr-2'/><a onClick= {() => navigate('/dashboard/home')}className='text-xl'>Home</a></div>
-               <div className =  {navItem('/dashboard/quests')}><FaClipboardQuestion className='size-6 mr-2' /><a onClick= {() => navigate('/dashboard/quests')} className='text-xl'> Quests</a></div>
-               <div className = {navItem('/dashboard/achivment')} ><FaTrophy className='size-6 mr-2'  /><a onClick= {() => navigate('/dashboard/achivment')}  className='text-xl'>Achivments</a></div>
-               <div className= {navItem('/dashboard/profile')}><IoMdSettings className='size-6 mr-2' /><a onClick= {() => navigate('/dashboard/profile')}  className='text-xl'>Profile</a></div>
+               <div onClick= {() => navigate('/dashboard/home')} className= {navItem('/dashboard/home')}> <IoMdHome className='size-6 mr-2'/><a className='text-xl'>Home</a></div>
+               <div onClick= {() => navigate('/dashboard/quests')} className =  {navItem('/dashboard/quests')}><FaClipboardQuestion className='size-6 mr-2' /><a  className='text-xl'> Quests</a></div>
+               <div onClick= {() => navigate('/dashboard/achivment')} className = {navItem('/dashboard/achivment')} ><FaTrophy className='size-6 mr-2'  /><a   className='text-xl'>Achivments</a></div>
+               <div onClick= {() => navigate('/dashboard/profile')}  className= {navItem('/dashboard/profile')}><IoMdSettings className='size-6 mr-2' /><a  className='text-xl'>Profile</a></div>
                <div className= 'mt-auto mb-5 flex items-center'><a className='mx-3'>{name}</a><button onClick={LogOut}>Sign Out</button></div>
             </div>
 
